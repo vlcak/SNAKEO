7865662df0e4fe1ddcaa988ab99ae6962ee30922
Jakub Vlcek <jakub.vlcek@gmail.com>
2024-05-22 10:46:01 +0200

    Add inheritance

--- src/main/java/entities/Enemy.java
+++ src/main/java/entities/Enemy.java
@@ -4,12 +4,7 @@ import main.GamePanel;
 
 import java.util.Random;
 
-public class Enemy {
-    int tileSize = 48;
-    int x;
-    int y;
-    int width = tileSize;
-    int height = tileSize;
+public class Enemy extends Body{
     Random random = new Random();
     public Enemy(){
         setDefaultValues();
@@ -17,37 +12,5 @@ public class Enemy {
     public void setDefaultValues(){
         x = random.nextInt(20) * tileSize;
         y = random.nextInt(20) * tileSize;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
     }
 }
--- src/main/java/entities/Head.java
+++ src/main/java/entities/Head.java
@@ -4,18 +4,10 @@ import main.GameFrame;
 import main.GamePanel;
 import main.KeyReader;
 
-public class Head {
+public class Head extends Body {
     GamePanel gamePanel;
-    int tileSize = 48;
-    int width = tileSize;
-    int height = tileSize;
-    int x;
-    int y;
-
-
 
     KeyReader keyReader = new KeyReader();
-
 
     public void setTileSize(int tileSize) {
         this.tileSize = tileSize;
@@ -44,38 +36,6 @@ public class Head {
         }
     }
 
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
     public int getTileSize() {
         return tileSize;
     }
